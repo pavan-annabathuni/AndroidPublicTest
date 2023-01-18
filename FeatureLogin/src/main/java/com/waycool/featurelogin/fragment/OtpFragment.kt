821ed0daa531b9cb7b33c9395c1d89fb2ff6de4b
pavan-annabathuni <pavan.annabathuni@waycool.in>
2023-01-18 16:09:53 +0530

    - Added Firebase Perf library to monitor performance - Added No device Card for irrigation Planner

--- FeatureLogin/src/main/java/com/waycool/featurelogin/fragment/OtpFragment.kt
+++ FeatureLogin/src/main/java/com/waycool/featurelogin/fragment/OtpFragment.kt
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
