4baeac8156099d1e9233b932b4896348695d7613
pavan-annabathuni <pavan.annabathuni@waycool.in>
2023-01-20 12:52:22 +0530

    pipeline

--- addcrop/src/main/java/com/example/addcrop/ui/premium/VarietyAdapter.kt
+++ addcrop/src/main/java/com/example/addcrop/ui/premium/VarietyAdapter.kt
@@ -29,9 +29,9 @@ private var row_index = -1
     }
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-        val details = varietyList[position]
+        val details = varietyList[holder.layoutPosition]
         holder.binding.tvSand.setText(varietyList[position].name)
-        if (row_index == position) {
+        if (row_index == holder.layoutPosition) {
             holder.binding.clSand.setBackgroundResource(R.drawable.bg_selected)
             holder.binding.ngClick.visibility= View.VISIBLE
 //            holder.binding.skillName.setTextColor(Color.parseColor("#FFFFFF"))
@@ -44,7 +44,7 @@ private var row_index = -1
         }
 //
         holder.binding.clSand .setOnClickListener {
-            row_index=position
+            row_index=holder.layoutPosition
             notifyDataSetChanged()
             itemSelectedListener.clickOnCategory(details)
             holder.binding.ngClick.visibility= View.GONE
