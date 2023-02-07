3846db6de631bc10c82d99c71c77d0fa19dc1a8c
mandviverma <mandvi.rakesh@waycool.in>
2023-02-07 21:30:44 +0530

    conflict

--- addfarm/src/main/java/com/waycool/addfarm/AddFarmActivity.kt
+++ addfarm/src/main/java/com/waycool/addfarm/AddFarmActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.waycool.featurelogin.deeplink.DeepLinkNavigator
+import com.waycool.featurelogin.deeplink.DeepLinkNavigator.navigateFromDeeplink
 
 class AddFarmActivity : AppCompatActivity() {
     lateinit var navHost: Fragment
@@ -15,7 +15,7 @@ class AddFarmActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_farm)
         navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)!!
-        DeepLinkNavigator.navigateFromDeeplink(this@AddFarmActivity) {pendingDynamicLinkData->
+      navigateFromDeeplink(this@AddFarmActivity) {pendingDynamicLinkData->
 
             var deepLink: Uri? = null
             if (pendingDynamicLinkData != null) {
