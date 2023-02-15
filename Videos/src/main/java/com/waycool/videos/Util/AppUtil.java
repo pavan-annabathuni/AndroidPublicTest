94b381472c9161bdaca177dfa3a55b9d88de00ca
mandviverma <mandvi.rakesh@waycool.in>
2023-02-15 15:57:57 +0530

    OD-2989 Code refactoring & optimization

--- Videos/src/main/java/com/waycool/videos/Util/AppUtil.java
+++ Videos/src/main/java/com/waycool/videos/Util/AppUtil.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 import com.waycool.videos.R;
 
@@ -41,11 +42,11 @@ public class AppUtil {
     }
 
     public static void showLoadImage(Context context, ImageView image) {
-//        try {
-//            Glide.with(context).asGif().load(R.raw.loading).into(image);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try {
+            Glide.with(context).asGif().load(R.raw.loading).into(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static String changeDateFormat(String givenDate) {
