b4c544293ccda68fef9400777ce7b6daf0bb69f9
mandviverma <mandvi.rakesh@waycool.in>
2023-02-20 22:56:44 +0530

    OD-3301 Premium - My Crops - Logic check for data highlighted in Green & Red color on homepage

--- app/src/main/java/com/waycool/iwap/premium/MyFarmPremiumAdapter.kt
+++ app/src/main/java/com/waycool/iwap/premium/MyFarmPremiumAdapter.kt
@@ -48,13 +48,18 @@ class MyFarmPremiumAdapter(val farmdetailslistener: Farmdetailslistener,val farm
         } else
             holder.binding.ivFeedback.visibility = View.GONE
 
-
         if (selectedFarmPosition == null) {
             selectedFarmPosition = position
             farmSelectedListener.onFarmSelected(detail)
         }
 
 
+//        val deviceList = deviceList.filter { it.farmId==detail.id }
+//        if(deviceList.isNullOrEmpty()){
+//            holder.binding.deviceIv.visibility=View.GONE
+//        }else{
+//            holder.binding.deviceIv.visibility=View.VISIBLE
+//        }
         if (position == selectedFarmPosition) {
             holder.binding.farmcl.setBackgroundResource(com.example.soiltesting.R.drawable.bg_add_form)
         } else {
@@ -73,26 +78,7 @@ class MyFarmPremiumAdapter(val farmdetailslistener: Farmdetailslistener,val farm
         holder.binding.viewFarmDetails.setOnClickListener {
             farmdetailslistener.onFarmDetailsClicked(detail)
         }
-
-
     }
-//    private fun getFarmLocation(lat: Double, lng: Double) {
-//        val geocoder = Geocoder(context, Locale.getDefault())
-//        val list: List<Address> =
-//            geocoder.getFromLocation(lat, lng, 1) as List<Address>
-//        district = list[0].locality + "," + list[0].adminArea
-//    }
-
-
-//    override fun onViewRecycled(holder: MyFarmPremiumViewHolder) {
-//        // Cleanup MapView here?
-//        holder.mapCurrent?.apply {
-//            clear()
-////            mapType = GoogleMap.MAP_TYPE_NORMAL
-////            loadFarm(details[],this)
-//        }
-//    }
-
     override fun getItemCount(): Int {
         return details.size
     }
@@ -105,63 +91,5 @@ class MyFarmPremiumAdapter(val farmdetailslistener: Farmdetailslistener,val farm
 
     inner class MyFarmPremiumViewHolder(val binding: ItemPremiumAddFarmBinding) :
         RecyclerView.ViewHolder(binding.root) {
-//        var mapCurrent: GoogleMap? = null
-//        var polygonCurrent: Polygon? = null
-//        var map: MapView? = null
-
-//        init {
-//            map = itemView.findViewById(R.id.map_premium_add_farm)
-//
-//            if (map != null) {
-//                map!!.onCreate(null)
-//                map!!.onResume()
-//                map!!.getMapAsync(this)
-//                map?.isClickable = false
-//            }
-//        }
-
-//        override fun onMapReady(googleMap: GoogleMap?) {
-//            MapsInitializer.initialize(context.applicationContext)
-//            mapCurrent = googleMap
-//            mapCurrent?.uiSettings?.setAllGesturesEnabled(false)
-//            mapCurrent?.uiSettings?.isMapToolbarEnabled = false
-//            loadFarm(details[layoutPosition].farmJson, mapCurrent)
-//
-//        }
-
-
-//        private fun loadFarm(farmJson: ArrayList<LatLng>?, mapCurrent: GoogleMap?) {
-//            mapCurrent?.mapType = GoogleMap.MAP_TYPE_NORMAL
-//            if (farmJson != null) {
-//                if (polygonCurrent != null)
-//                    polygonCurrent?.remove()
-//                if (farmJson.size >= 3) {
-//                    polygonCurrent = mapCurrent?.addPolygon(
-//                        PolygonOptions().addAll(farmJson).fillColor(Color.argb(100, 58, 146, 17))
-//                            .strokeColor(
-//                                Color.argb(255, 255, 255, 255)
-//                            )
-//                    )
-//
-//                    mapCurrent?.animateCamera(
-//                        CameraUpdateFactory.newLatLngBounds(
-//                            getLatLnBounds(farmJson), 10
-//                        )
-//                    )
-//                } else {
-//                    mapCurrent?.animateCamera(CameraUpdateFactory.newLatLngZoom(farmJson[0], 16f))
-//                }
-//            }
-//
-//        }
-
-//        private fun getLatLnBounds(points: List<LatLng?>): LatLngBounds? {
-//            val builder = LatLngBounds.builder()
-//            for (ll in points) {
-//                builder.include(ll)
-//            }
-//            return builder.build()
-//        }
-
-    }
+        }
 }
